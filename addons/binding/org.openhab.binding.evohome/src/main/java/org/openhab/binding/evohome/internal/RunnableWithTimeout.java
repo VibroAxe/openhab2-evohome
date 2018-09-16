0c2c5d7219e559df1252effa175fd6de0869bfe2
Unknown <jvanzuijlen@gmail.com>
2018-09-16 14:00:51 +0200

    docs: RunnableWithTimeout

--- addons/binding/org.openhab.binding.evohome/src/main/java/org/openhab/binding/evohome/internal/RunnableWithTimeout.java
+++ addons/binding/org.openhab.binding.evohome/src/main/java/org/openhab/binding/evohome/internal/RunnableWithTimeout.java
@@ -2,6 +2,12 @@ package org.openhab.binding.evohome.internal;
 
 import java.util.concurrent.TimeoutException;
 
+/**
+ * Provides an interface for a delegate that can throw a timeout
+ * 
+ * @author Jasper van Zuijlen - Initial contribution
+ *
+ */
 public interface RunnableWithTimeout {
 
     public abstract void run() throws TimeoutException;
